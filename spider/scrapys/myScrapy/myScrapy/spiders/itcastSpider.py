7038d7fe4d3978cf049e3cb0d0fb2247a9039aee
USER-20170626LB\Administrator <867999030@qq.com>
2017-12-18 22:31:31 +0800

    no message

--- spider/scrapys/myScrapy/myScrapy/spiders/itcastSpider.py
+++ spider/scrapys/myScrapy/myScrapy/spiders/itcastSpider.py
@@ -1,11 +1,29 @@
 # -*- coding: UTF-8 -*-
 import scrapy
+from myScrapy.items import ItcastItem
 
 class ItcastSpider(scrapy.Spider):
-    name = 'itcast'
-    allowed_domains = ['http://www.itcast.cn/']
-    start_urls = ['http://www.itcast.cn/channel/teacher.shtml#apython']
+    name = 'itcast'   #爬虫名字
+    allowed_domains = ['http://www.itcast.cn/']  #作用域
+    start_urls = ['http://www.itcast.cn/channel/teacher.shtml#apython']  #爬取地址
 
     def parse(self, response):
-        with open('teachers.html', 'w+') as f:
-            f.write(response.body)
+
+        teacher_list = response.xpath('//div[@class="li_txt"]')
+        teacherItem = []
+        for each in teacher_list:
+            item = ItcastItem() #实例化一个ItcastItem对象，对象包含items.py文件里的字段
+            #通过scrapy自带xpath匹配出所有根节点列表集合
+            #extract() 用于将匹配的结果转换成unicode字符串
+            name = each.xpath('./h3/text()').extract()
+            title = each.xpath('./h4/text()').extract()
+            desc = each.xpath('./p/text()').extract()
+            # 赋值三个字段
+            item['name'] = name[0]
+            item['title'] = title[0]
+            item['desc'] = desc[0]
+
+            teacherItem.append(item)
+        #爬取结果写入文件
+        with open('teachers.txt', 'w+', encoding='utf8') as f:
+            f.write(str(teacherItem))
