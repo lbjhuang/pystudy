4e8bebc6b5a571a368eecb91395fdc083021cb06
lbjhuang <867999030@qq.com>
2018-04-19 17:23:46 +0800

    no message

--- others/coppydemo.py
+++ others/coppydemo.py
@@ -3,7 +3,7 @@ import copy
 
 
 b = [1,2,1]
-#浅拷贝，指向统一内存地址，共用一份数据，任何一方更改，会影响另一个
+#直接赋值，指向统一内存地址，共用一份数据，任何一方更改，会影响另一个
 a = b
 print(id(a))
 print(id(b))
@@ -11,6 +11,17 @@ print(id(b))
 a.append(4)
 print(b)
 print(a)
+
+
+l = [1,2,1]
+#浅拷贝 开辟不同地址存放数据，只拷贝一层数据
+m = copy.copy(l)
+print(id(l))
+print(id(m))
+
+l.append(4)
+print(m)
+print(l)
 
 #深拷贝，如果不同的内存地址，一步一步取得数据，在别的内存地址存起来，其他和原来内存地址的没啥关系了
 c = copy.deepcopy(a)
@@ -27,21 +38,26 @@ g = [e,f]
 h = copy.deepcopy(g)
 e.append(9) # [1,2,3,9]
 print(h)    #h 依旧是[[1, 2, 3], [4, 5, 6]]
+print(g)
 
 #copy和deepcopy都会新开辟内存
-#copy 只拷贝一层，如果copy的是引用，则是引用，据修改也会源数跟着改变，如果是copy的是数据则是新存自己一份数据。
+#copy 只拷贝一层，如果copy的是引用，则是引用，数据修改也会源数跟着改变，如果是copy的是数据则是新存自己一份数据。
 #例如：m列表保存了j,k的引用，l用copy.copy(m)则是新建内存地址，拷贝m一次。发现m里面是引用的j,k的内存数据，所以l里面也存着指向j,k的内存数据, 当源数据j添加一个4，则l也会跟着改变添加了一个4。
-#     q是j的一层拷贝，发现j里面直接就是数据了，所有会新存自己的一份数据，当j改变时，q不会改变了
+#     s是j的一层拷贝，发现j里面直接就是数据了，所有会新存自己的一份数据，当j改变时，q不会改变了
 j = [1,2,3]
 k = [7,8,9]
 m = [j,k]
+
 l = copy.copy(m)
-q = copy.copy(j)
+s = copy.copy(j)
+q = copy.deepcopy(m)
+
 print(id(l))
-print(id(j))
+print(id(q))
 j.append(4)
 print(l)   #[[1, 2, 3, 4], [7, 8, 9]]
-print(q)   #[1, 2, 3]
+print(q)   #[[1, 2, 3], [7, 8, 9]]
+print(s)   #[1, 2, 3]
 
 
 
