35459423613018c371a3c2e452b843a6f8e51772
lbjhuang <867999030@qq.com>
2018-04-25 15:17:51 +0800

    no message

--- mokuai/jsonl.py
+++ mokuai/jsonl.py
@@ -10,6 +10,15 @@ print(j)
 s1 = {"age":"11", "his":"22", "sam":"44"}
 k = json.dumps(s1)  #把基本数据dict类型转换成json字符串
 
+with open("test.txt","w") as f:  #以普通模式写入
+    f.write(json.dumps(s1)) #把内存对象转为字符串
+
+with open("test.txt","rb") as f2:
+    ages = json.loads(f2.read()) #把数据重新载入
+
 print(type(k))
 print(k)
+print(ages)
+
+
 
